b8a8ff7fddb88a45cb130187523838e966c486ac
Gefferson-Souza <geffersonteodorodesouza@gmail.com>
2025-04-07 19:46:04 -0300

    feat: add users, Patch, Put, Delete

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,28 +1,49 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
-
   @Post()
-  async create(@Body() body:any): Promise<any> {
-    // Simulate a user creation process
-    // In a real application, you would save the user to the database here
+  async create(@Body() body: any): Promise<any> {
     console.log('User created:', body);
     return body;
   }
 
   @Get()
   async read(): Promise<any> {
-    return {users:[]};
+    return { users: [] };
   }
 
   @Get(':id')
-  async readOne(@Param() id:any): Promise<any> {
-    const user = {
-        ...id
-    }
-    return user;
+  async readOne(@Param() params): Promise<any> {
+    return {
+      user: {},
+      params,
+    };
   }
 
+  @Put(':id')
+  async update(@Body() body, @Param() params): Promise<any> {
+    return {
+      user: {},
+      params,
+      body,
+    };
+  }
 
+  @Patch(':id')
+  async updatePartial(@Body() body, @Param() params): Promise<any> {
+    return {
+      user: {},
+      params,
+      body,
+    };
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param() params): Promise<any> {
+    return {
+      user: {},
+      params,
+    };
+  }
 }
